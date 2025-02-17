ad177d394491b4647fe47c8b23c642b03bf3d19f
MPD97 <mateuszpracadom@gmail.com>
2025-02-17 12:11:47 +0100

    fix failing build

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders project sections', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const projectElement = screen.getByText(/Projekt 1/i);
+  expect(projectElement).toBeInTheDocument();
 });
