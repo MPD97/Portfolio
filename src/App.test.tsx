4924b9e883735de0ce26aa02b188ff0729de9d9e
MPD97 <mateuszpracadom@gmail.com>
2025-02-21 08:37:40 +0100

    start again

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders project sections', () => {
-  render(<App />);
-  const projectElement = screen.getByText(/Projekt 1/i);
-  expect(projectElement).toBeInTheDocument();
-});
